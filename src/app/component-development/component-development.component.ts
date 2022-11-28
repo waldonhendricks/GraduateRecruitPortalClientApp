4311d44d5f130d5a41ab68980b6434713497eb16
Mbuso Kotobe <mbuso.kotobes@gmail.com>
2022-11-28 17:21:13 +0200

    TASK: Added direct vacancy job component in component.html

---
+++ src/app/component-development/component-development.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-component-development',
+  templateUrl: './component-development.component.html',
+  styleUrls: ['./component-development.component.css']
+})
+export class ComponentDevelopmentComponent {
+
+}
