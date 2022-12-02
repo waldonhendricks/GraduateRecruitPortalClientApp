010a54a8eb5cd1e1ecda8b4f619c65d0ff4344a5
Thina Mzosindiso Nontwabaza <219189153@mycput.ac.za>
2022-12-02 06:01:42 +0200

    First commit

---
+++ src/app/forgot-password/forgot-password.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-forgot-password',
+  templateUrl: './forgot-password.component.html',
+  styleUrls: ['./forgot-password.component.css']
+})
+
+export class ForgotPasswordComponent implements OnInit{
+  email: new FormControl("");
+}
