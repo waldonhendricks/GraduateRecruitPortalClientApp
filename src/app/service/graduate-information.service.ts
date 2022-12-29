5f29c8e0652b1ba031ad15630ff72e26e26a099b
Mbuso Kotobe <mbuso.kotobes@gmail.com>
2022-12-30 00:52:49 +0200

    PROJECT UPDATE: Moved the reset-password service to the service directory

--- src/app/service/graduate-information.service.ts
+++ src/app/service/graduate-information.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
-import { Graduate } from './model/graduate';
+import { Graduate } from '../model/graduate';
 
 
 @Injectable({
